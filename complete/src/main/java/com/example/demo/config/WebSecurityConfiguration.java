c7ae0d94de169cb6542ec9d3ba25667d93232ec3
LUIS FELIPE URBINA MORENO <LUIZ-FELIPE16@HOTMAIL.COM>
2019-12-19 22:58:35 +0000

    gitpod restart

--- complete/src/main/java/com/example/demo/config/WebSecurityConfiguration.java
+++ complete/src/main/java/com/example/demo/config/WebSecurityConfiguration.java
@@ -11,31 +11,44 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@ImportResource({"classpath:bsis-servlet.xml"})
 
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("test").password("password").roles("USER");
-    }
-
-    @Bean
-    public AuthenticationEntryPoint authenticationEntryPoint() {
-        return (request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+@Override
+    protected void configure(AuthenticationManagerBuilder auth)
+      throws Exception {
+        auth
+          .inMemoryAuthentication()
+          .withUser("user")
+            .password("password")
+            .roles("USER")
+            .and()
+          .withUser("admin")
+            .password("admin")
+            .roles("USER", "ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-        .authorizeRequests()
-        .anyRequest()
-        .authenticated()
-        .and()
-        .httpBasic();
-
+          .authorizeRequests()
+          .anyRequest()
+          .authenticated()
+          .and()
+          .httpBasic();
     }
+
 }
